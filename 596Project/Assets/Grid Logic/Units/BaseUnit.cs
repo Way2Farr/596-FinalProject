1e7da5775fe0b097e0bb19069807ee973b41e878
oyasumiku <98620862+oyasumiku@users.noreply.github.com>
2025-05-03 22:28:03 -0700

    partially animated characters

--- 596Project/Assets/Grid Logic/Units/BaseUnit.cs
+++ 596Project/Assets/Grid Logic/Units/BaseUnit.cs
@@ -9,7 +9,12 @@ public class BaseUnit : MonoBehaviour
     
     public Tile OccupiedTile;
     public Faction Faction;
-
+    public Animator _unitAnimator;
+    public SpriteRenderer _spriteRenderer;
+    static readonly int IsIdle = Animator.StringToHash("IsIdle");
+    static readonly int IsMoving = Animator.StringToHash("IsMoving");
+    static readonly int IsAttacking = Animator.StringToHash("IsAttacking");
+    static readonly int IsDamaged = Animator.StringToHash("IsDamaged");
     [Header("UI Elements")]
     public Canvas healthbarCanvas;
 
@@ -20,7 +25,11 @@ public class BaseUnit : MonoBehaviour
 
     [SerializeField]
     private string _name;
-
+    public void Awake()
+    {
+        _unitAnimator = GetComponentInChildren<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     public int getMovementRange()
     {
         return _movementRange;
@@ -54,5 +63,17 @@ public class BaseUnit : MonoBehaviour
         List<Tile> _inRangeTiles = GridManager.Instance._tiles.Values.Where(t => Vector2.Distance(this.transform.position, t.transform.position) <= tempRange).ToList();
 
         return _inRangeTiles;
+    }
+
+    public void startMoving ()
+    {
+        _unitAnimator.SetBool(IsIdle, false);
+        _unitAnimator.SetBool(IsMoving, true);
+    }
+
+    public void stopMoving ()
+    {
+        _unitAnimator.SetBool(IsIdle, true);
+        _unitAnimator.SetBool(IsMoving, false);
     }
 }
