52bf3bac052ea7da7ba65e12b8bb29d4e00b74cf
oyasumiku <98620862+oyasumiku@users.noreply.github.com>
2025-05-12 13:04:28 -0700

    merges finalized

--- 596Project/Assets/DataCarry.cs
+++ 596Project/Assets/DataCarry.cs
@@ -16,6 +16,7 @@ public class DataCarry : MonoBehaviour
 
         if (statManager != null)
         {
+            Debug.Log("Stat manager found.");
             DontDestroyOnLoad(statManager);
         }
         
