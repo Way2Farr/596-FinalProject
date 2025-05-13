95bd10e9c71beab56e947e2c47975a2b180365d7
oyasumiku <98620862+oyasumiku@users.noreply.github.com>
2025-05-13 14:01:06 -0700

    Update Enemy1.cs

--- 596Project/Assets/Grid Logic/Units/Enemy1.cs
+++ 596Project/Assets/Grid Logic/Units/Enemy1.cs
@@ -19,16 +19,19 @@ public class Enemy1 : BaseEnemy
     public override List<Tile> getMovementTiles()
     {
         float tempRange = this.getMovementRange();
-        List<Tile> _inRangeTiles = GridManager.Instance._tiles.Values.Where(t => t._position.x == this.OccupiedTile._position.x).ToList();
+        List<Tile> _inRangeTiles = GridManager.Instance._tiles.Values.Where(t => Vector2.Distance(this.transform.position, t.transform.position) <= tempRange && t._position != OccupiedTile._position).ToList();
 
         return _inRangeTiles;
     }
 
     public override List<Tile> getAttackTiles()
     {
+
         float tempRange = this.getAttackRange();
-        List<Tile> _inRangeTiles = GridManager.Instance._tiles.Values.Where(t => Vector2.Distance(this.transform.position, t.transform.position) <= tempRange && t._position != OccupiedTile._position).ToList();
+        List<Tile> _inRangeTiles = GridManager.Instance._tiles.Values.Where(t => t._position.x == this.OccupiedTile._position.x || t._position.y == this.OccupiedTile._position.y).ToList();
 
         return _inRangeTiles;
+
+        
     }
 }
